3a14aea615e95a85b50b5dac7270d2a55f8d9bf8
Andrei Buga <73544964+Andre1z@users.noreply.github.com>
2025-05-11 15:51:24 +0200

    2

--- change_department.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['department_id']);
-header("Location: index.php");
-exit;
-?>
--- config.php
+++
@@ -1,5 +0,0 @@
-<?php
-$config = [
-    'db_name' => 'data_migrator.db'
-];
-?>
